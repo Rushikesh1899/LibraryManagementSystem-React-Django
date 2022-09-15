c55e51390495c92028c171c6e08997efbfd4bf12
rushigurav9189 <rushigurav9189@gmail.com>
2022-09-15 10:41:40 +0530

    first commit

---
+++ clientside/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AddBooks from "./components/AddBooks";
+
+function App() {
+  return (
+    <div className="App">
+      <AddBooks />
+    </div>
+  );
+}
+
+export default App;
